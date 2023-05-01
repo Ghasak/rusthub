a4c07eb808bacabb5c673e6950fbb4017a818469
Ghasak <mdotgh@gmail.com>
2023-05-02 00:59:35 +0900

    [generic] in progress ..

--- docs/RustCore/rust_generic.md
+++ docs/RustCore/rust_generic.md
@@ -90,3 +90,70 @@ pub fn generic_in_rust_concept() {
     println!("{point_2:#?}")
 }
 ```
+
+
+- The `T` is a placeholder until the complier saw you using in explicit manner.
+  Once it understand your intend for a piece of data, it's off to the races,
+  and treat it as an explici type within your code from there on out. You're
+  able to create any data you like that fits the generic defintion given you
+  comply with some constraints which we will talk about shortly.
+
+- So, what are `Generic` and why you would like to use them. `Generic` is
+  available in other languages and certainly aren't unique to `Rust`, it's a
+  way of creating abstarct types both for both `struct`, `enum` and also for
+  `function, methods`. We're really creating a placeholder types instead of
+  explicityly defining `i32`, `String` , `&str` or whatever. It says' I am
+  going to write a code that can work for multiple different types but I don't
+  want to have to write individual code for each type I want to handle. This
+  will add a significant flexibility not only for your current coding needs,
+  but also for future use, so basically:
+    - Abstract Types (aka placeholder types)
+    - Adds flexibility
+    - Reduces Code Duplciation.
+    - No runtime cost of using `Generic`.
+
+
+- You can use the `T` for first Generric. And, if we want another Generic
+  variable we can add as many as we need, next `U` can be used.
+
+- You can also constrcut your `Generic` with strict defintion to some type for
+  example see below. That we can add an explicit defintion to the data we need.
+
+```rust
+struct MyInfo<T>{
+        x: i32,
+        y: T,
+        z: T,
+        some_char: char,
+
+    }
+
+```
+
+
+- I only make things generic if you have a need to support multiple types, it's
+  too easy to go down the abastrct rabbit hole until nobady knows what the hell
+  your code is trying to do, let's cleanup for the next section.
+
+
+- `Generic` with `Enum`,
+
+```rust
+pub fn generic_in_rust_concept() {
+    #[derive(Debug)]
+    enum RandomInfo<T> {
+        OptionA(T),
+        OptionB(T),
+        OptionC,
+    }
+
+    let my_data: RandomInfo<f32> = RandomInfo::OptionA(100.23);
+    let my_other_data: RandomInfo<f64> = RandomInfo::OptionB(400.23);
+    let last_data: RandomInfo<f32> = RandomInfo::OptionC;
+
+
+    let some_option:RandomInfo<&str> = RandomInfo::OptionA("This is just  a test ...");
+
+    println!("{my_data:#?}");
+    println!("{some_option:#?}");
+}```
--- src/concepts/ch03/generic_in_rust.rs
+++ src/concepts/ch03/generic_in_rust.rs
@@ -1,28 +1,66 @@
+use crate::concepts::ch03::structs_traits_and_implementation::RandomInfo;
+use itertools::Itertools;
+
 pub fn generic_in_rust_concept() {
     #[derive(Debug)]
-    struct Point<T, U> {
-        x: T,
-        y: U,
+    enum RandomInfo<T> {
+        OptionA(T),
+        OptionB(T),
+        OptionC,
     }
 
-    let point_1 = Point {
-        x: 1012.23,
-        y: 1121.44,
-    };
+    let my_data: RandomInfo<f32> = RandomInfo::OptionA(100.23);
+    let my_other_data: RandomInfo<f64> = RandomInfo::OptionB(400.23);
+    let last_data: RandomInfo<f32> = RandomInfo::OptionC;
+    let some_option: RandomInfo<&str> = RandomInfo::OptionA("This is just  a test ...");
+    let some_vec: RandomInfo<Vec<String>> =
+        RandomInfo::OptionA(vec!["G".to_owned(), "H".to_owned()]);
 
-    let point_2 = Point { x: 12, y: 10 };
+    println!("{my_data:#?}");
+    println!("{some_vec:#?}");
 
-    let point_2 = Point { x: 12.0, y: 10 };
+    let input_x: [i32; 5] = [100, 200, 300, 400, 500];
+    let output = experiment_function(&input_x);
+    println!("{output:#?}");
 
-    let point_2 = Point { x: 12, y: 10.12 };
-    // Accept float
-
-    println!("{point_1:#?}");
-    // Accept Int
-    println!("{point_2:#?}");
-
-    for i in 0..10 {
-        println!("The value of i -> {i}")
-
+    let our_set: &[char; 4] = &['A', 'B', 'C', 'D'];
+    // use itertools::Itertools;
+    // let permutations = our_set.iter().permutations(2);
+    // for val in permutations {
+    //     println!("{val:?}");
+    // }
+    let output = give_me_my_permutations(our_set, 3);
+    match output {
+        Ok(val) => println!("{val:?}"),
+        Err(message) => (),
     }
+}
+
+#[allow(unused_assignments)]
+pub fn experiment_function(param_a: &[i32; 5]) -> Vec<String> {
+    let mut out_put_vec: Vec<String> = Vec::with_capacity(5);
+    let mut output_message: String = String::new();
+    for val in param_a {
+        if val.to_string() == "100" {
+            output_message = "This is $100".to_owned();
+            out_put_vec.push(output_message);
+        } else {
+            output_message = format!("This is not acceptable {val}");
+            out_put_vec.push(output_message);
+        }
+    }
+    out_put_vec
+}
+
+/// Permutations and combination function
+/// Input:
+/// - `param_a`: `&[char]` -> Given the input of vector of characters.
+pub fn give_me_my_permutations(param_a: &[char], k: usize) -> Result<Vec<Vec<&char>>, String> {
+    let mut output_vec: Vec<Vec<&char>> = Vec::new();
+    let permutations = param_a.iter().permutations(k);
+    for val in permutations {
+        println!("{:?}", val);
+        output_vec.push(val)
+    }
+    Ok(output_vec)
 }
